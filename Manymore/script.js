4ca5be00eec30a90240e172210c93eba7b82c57e
AbdulazizYas <abdulazizyass@gmail.com>
2022-08-22 23:05:33 +0300

    add manymore files

--- Manymore/script.js
+++ Manymore/script.js
@@ -1,5 +1,5 @@
 $("document").ready(function () {
-  $("footer .row.lined").append(
+  $(".social-footer").append(
     '<a href="https://maroof.sa/78399" class="maarof" target="_blank"><img border="0" alt="Maarof" src="https://elitefood.com.sa/image/ZSGOZ908/data/maarof.png"></a>'
   );
 });
